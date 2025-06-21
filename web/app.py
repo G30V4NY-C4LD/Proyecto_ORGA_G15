ef22a4127727e7fed4a3e39e9698f2726095f2b1
Geovany <g.j.c.d2017@gmail.com>
2025-06-20 20:04:28 -0600

    commenting code of app.py

--- web/app.py
+++ web/app.py
@@ -1,3 +1,4 @@
+# import de las librerias a usar
 from flask import Flask, render_template, jsonify
 import serial
 import threading
@@ -8,9 +9,9 @@ app = Flask(__name__)
 # Variables globales
 ocupados = 0
 libres = 0
-espacios = ["desconocido"] * 4
-arduino_disponible = False
-ultimo_mensaje = ""
+espacios = ["desconocido"] * 4 # lista para los espacios
+arduino_disponible = False # variable booleana para poder saber si el arduiono se conecto
+ultimo_mensaje = "" # aguarda el ultimo mensaje para ver si hay cambio despues 
 
 # Intento de conexión al Arduino
 try:
@@ -20,7 +21,7 @@ try:
 except:
     print("Arduino no disponible")
 
-# Función que lee el serial continuamente
+# Función que lee el puerto serial 
 def leer_serial():
     global ocupados, libres, espacios, arduino_disponible, ultimo_mensaje
 
@@ -28,17 +29,16 @@ def leer_serial():
         if arduino_disponible and arduino.in_waiting:
             try:
                 linea = arduino.readline().decode().strip().lower()
-                print("Recibido del serial:", linea)
-                if linea and linea != ultimo_mensaje and "ocupados" in linea and "libres" in linea:
-                    print("Recibido del serial:", linea)
+                if linea and linea != ultimo_mensaje and "ocupados" in linea and "libres" in linea: # que hay cambio en con respecto al ultimo estado, y que resibe en formato 'ocupado:#, libre:#'
+                    print("Recibido del serial:", linea) # en terminal el contenido del serial
                     ultimo_mensaje = linea
 
-                    partes = linea.replace(" ", "").split(",")
+                    partes = linea.replace(" ", "").split(",") # separa en partes lo de ocupado y libres para tomar los numeros y mandarlos al frontend
                     ocupados = int(partes[0].split(":")[1])
                     libres = int(partes[1].split(":")[1])
-                    espacios = ["ocupado"] * ocupados + ["libre"] * libres
+                    espacios = ["ocupado"] * ocupados + ["libre"] * libres #llena los espacios con ocupado o libre
                     while len(espacios) < 4:
-                        espacios.append("desconocido")
+                        espacios.append("desconocido") # si no sabe si esta libre u ocupado, ya sea porque el serial no se esta lleyendo bien
             except Exception as e:
                 print("Error al procesar:", e)
         time.sleep(1)
@@ -51,7 +51,7 @@ def Home_web():
     return render_template("index.html")
 
 @app.route("/estado_espacios")
-def estado_espacios():
+def estado_espacios():  # los datos del formato json para mostrar el estado de los parqueos, y el total
     return jsonify({
         "ocupados": ocupados,
         "libres": libres,
